7f69326ba1e789376821262d0c557dcbeb6f376e
eddesks <ericdenniso@gmail.com>
2024-01-17 15:49:10 -0300

    componente  view/header criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { Componente1Component } from './componente1/componente1.component';
 import { Componente2Component } from './componente2/componente2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './views/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Componente1Component,
-    Componente2Component
+    Componente2Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
