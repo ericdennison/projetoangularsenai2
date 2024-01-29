65f53ca7df664a42739f0e829b508c8a7692fe84
eddesks <ericdenniso@gmail.com>
2024-01-29 15:02:25 -0300

    modificação feita nos arquivos dbgames.json atualiza-produto(html e ts) cadastro-produto(html, css e ts, lista-produto(html e ts)

--- src/app/restrito/cadastro-produto/cadastro-produto.component.ts
+++ src/app/restrito/cadastro-produto/cadastro-produto.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../../../../src/app/models/Produto.models';
+import { ProdutoService } from '../../../../src/app/produto.service';
 @Component({
   selector: 'app-cadastro-produto',
   templateUrl: './cadastro-produto.component.html',
-  styleUrl: './cadastro-produto.component.css'
+  styleUrls: ['./cadastro-produto.component.css']
 })
-export class CadastroProdutoComponent {
+export class CadastroProdutoComponent  {
+  public produto: Produto = new Produto(0,"","","",0);
+  constructor(private _produtoService: ProdutoService, private router: Router) { }
+  
 
+  cadastrar(): void {
+    this._produtoService.cadastrarProduto(this.produto).subscribe(
+      produto => {
+        this.produto = new Produto(0,"","","",0);
+        alert("Cadastro Efetuado com Sucesso")
+      },
+      err => {
+        alert("erro ao cadastrar")
+      }
+    );
+    this.router.navigate(["/restrito/lista"]);
+  }
 }
--- src/app/restrito/lista-produto/lista-produto.component.ts
+++ src/app/restrito/lista-produto/lista-produto.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../../../../src/app/models/Produto.models';
+import { ProdutoService } from '../../produto.service';
 @Component({
   selector: 'app-lista-produto',
   templateUrl: './lista-produto.component.html',
-  styleUrl: './lista-produto.component.css'
+  styleUrls: ['./lista-produto.component.css']
 })
-export class ListaProdutoComponent {
-
+export class ListaProdutoComponent implements OnInit {
+  public produtos: Produto[] = [];
+  constructor(private _produtoService: ProdutoService,private router: Router) { }
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+  listarProdutos(): void {
+    this._produtoService.getProdutos()
+      .subscribe(
+        retornaProduto => {
+          this.produtos = retornaProduto.map(
+            item => {
+              return new Produto(
+                item.id,
+                item.produto,
+                item.descricao,
+                item.foto,
+                item.preco
+              );
+            }
+          )
+        }
+      )
+  }
+  excluir(id: number) {
+    this._produtoService.removerProduto(id).subscribe(
+      produto => {
+        this.listarProdutos();
+      },
+      err => { alert("Erro ao Excluir") }
+    );
+    // window.location.href = "/restrito/lista";
+    this.router.navigate(["/restrito/lista"]);
+  }
 }
