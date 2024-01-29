24f1b5aeaac9ee80808953c02f458ef8d393d6a2
ericdennison <ericdenniso@gmail.com>
2024-01-28 22:53:07 -0300

    delete produto id 4 dbgames.json | criado componente-restrito(lista, atualiza, cadastro, menu-produto)

---
+++ src/app/restrito/menu-produto/menu-produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-produto',
+  templateUrl: './menu-produto.component.html',
+  styleUrl: './menu-produto.component.css'
+})
+export class MenuProdutoComponent {
+
+}
